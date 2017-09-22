b31aec5f0680b16c3466550db76f85afcf716b49
cry101 <friend3221>
2017-09-22 17:00:23 +0800

    1

--- src/views/lottery/index2.vue
+++ src/views/lottery/index2.vue
@@ -1,72 +1,54 @@
 <template>
   <div class="app-container">
-    <el-table :data="list" v-loading.body="listLoading" element-loading-text="拼命加载中" border fit highlight-current-row>
-      <el-table-column align="center" label='ID' width="95">
-        <template scope="scope">
-          {{scope.$index}}
-        </template>
-      </el-table-column>
-      <el-table-column label="Title">
-        <template scope="scope">
-          {{scope.row.title}}
-        </template>
-      </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
-        <template scope="scope">
-          <span>{{scope.row.author}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
-        <template scope="scope">
-          {{scope.row.pageviews}}
-        </template>
-      </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
-        <template scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{scope.row.status}}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
-        <template scope="scope">
-          <i class="el-icon-time"></i>
-          <span>{{scope.row.display_time}}</span>
-        </template>
-      </el-table-column>
-    </el-table>
+    <el-form :model="addForm" ref="addForm" label-width="100px">
+      <el-form-item label="期数">
+        <el-input type="text" v-model="addForm.qihao" auto-complete="off" style="width: 300px"></el-input>
+      </el-form-item>
+      <el-form-item label="第一球">
+        <el-input type="text" v-model="addForm.num1" auto-complete="off" style="width: 300px"></el-input>
+      </el-form-item>
+      <el-form-item label="第二球">
+        <el-input type="text" v-model="addForm.num2" auto-complete="off" style="width: 300px"></el-input>
+      </el-form-item>
+      <el-form-item label="第三球">
+        <el-input type="text" v-model="addForm.num3" auto-complete="off" style="width: 300px"></el-input>
+      </el-form-item>
+      <el-form-item label="第四球">
+        <el-input type="text" v-model="addForm.num4" auto-complete="off" style="width: 300px"></el-input>
+      </el-form-item>
+      <el-form-item label="第五球">
+        <el-input type="text" v-model="addForm.num5" auto-complete="off" style="width: 300px"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('addForm')">提交</el-button>
+        <el-button @click="resetForm('addForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
-import { getList } from '@/api/table'
 
 export default {
   data() {
     return {
-      list: null,
-      listLoading: true
-    }
-  },
-  filters: {
-    statusFilter(status) {
-      const statusMap = {
-        published: 'success',
-        draft: 'gray',
-        deleted: 'danger'
+      addForm: {
+        qihao: '',
+        num1: '',
+        num2: '',
+        num3: '',
+        num4: '',
+        num5: ''
       }
-      return statusMap[status]
-    }
+    };
   },
   created() {
-    this.fetchData()
   },
   methods: {
-    fetchData() {
-      this.listLoading = true
-      getList(this.listQuery).then(response => {
-        this.list = response.data.items
-        this.listLoading = false
-      })
-    }
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    submitForm(formName){}
   }
 }
 </script>
